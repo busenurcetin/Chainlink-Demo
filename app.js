96b308ce3c38eeab93c7849c826c7d4c901b10ae
Buse Nur <110244548+busenurcetin@users.noreply.github.com>
2023-05-21 13:46:43 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+/* To connect using MetaMask */
+async function connect() {
+  if (window.ethereum) {
+     await window.ethereum.request({ method: "eth_requestAccounts" });
+     window.web3 = new Web3(window.ethereum);
+     const account = web3.eth.accounts;
+     //Get the current MetaMask selected/active wallet
+     const walletAddress = account.givenProvider.selectedAddress;
+     console.log(`Wallet: ${walletAddress}`);
+  
+  } else {
+   console.log("No wallet");
+  }
+}
